c1bf5f143088626337c3f3ceb684f90b30768a82
rangerdong <844199574@qq.com>
2025-12-28 20:17:22 +0800

    优化算法执行

--- algo_server.py
+++ algo_server.py
@@ -10,12 +10,12 @@ import uvicorn
 from dotenv import load_dotenv
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from algorithm import load_predictors,load_predictor,setup_predictors
 from celery_app import celery_app
-from db import Session, Signal
-from services import rtsp, task, algo
+from services import algo
 from setup import setup_logging
 logger = setup_logging('algo_server', log_file='algo_server-2.log')
 # 在Flask启动前设置
@@ -28,7 +28,12 @@ pathway_intervals = {}
 interval = 5
 # PREDICTORS = load_predictor()
 # PREDICTORS = None
-
+origins = [
+    "https://video-center.hibuilding.cn",
+    "http://10.1.28.100:10990",
+    "https://v.qq.com",
+    "https://www.iqiyi.com"
+]
 
 @asynccontextmanager
 async def lifespan(fastapi: FastAPI):
@@ -41,6 +46,13 @@ async def lifespan(fastapi: FastAPI):
     yield
 app = FastAPI(lifespan=lifespan)
 app.include_router(algo.router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.exception_handler(Exception)
 async def http_exception_handler(request: Request, exc: Exception):
